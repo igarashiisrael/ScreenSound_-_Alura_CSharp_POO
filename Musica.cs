cd867aeacf3aa868c56492a195711802d838f63b
Israel Igarashi <89955427+igarashiisrael@users.noreply.github.com>
2023-08-28 12:49:24 -0300

    first commit
    Projeto inicial, criado num curso anterior de c#.

---
+++ Musica.cs
@@ -0,0 +1,32 @@
+﻿class Musica
+{
+    public Musica(Banda artista, string nome)
+    {
+        Artista = artista;
+        Nome = nome;
+    }
+    public string Nome { get; set; }
+    public Banda Artista { get; }
+    public int Duracao { get; set; }
+    public bool Disponivel { get; set; }
+    public string DescricaoResumida =>
+        $"A música {Nome} pertence à banda {Artista} e tem duração de aproximadamente {Duracao} minutos.";
+
+
+
+    public void ExibirFichaTecnica()
+    {
+        Console.WriteLine($"Nome da música: {Nome}");
+        Console.WriteLine($"Artista: {Artista.Nome}");
+        Console.WriteLine($"Duração: {Duracao}");
+        if (Disponivel)
+        {
+            Console.WriteLine("Disponível no plano!");
+        }
+        else
+        {
+            Console.WriteLine("Não disponível no plano!");
+        }
+        Console.WriteLine($"Descrição: {DescricaoResumida}");
+    }
+}
